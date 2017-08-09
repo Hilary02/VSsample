3d266b278fa1886cbfe7fd2075caccddd4555379
Ryota Ehira <c011605154@edu.teu.ac.jp>
2017-08-09 23:32:35 +0900

    [add]数字の入出力

--- Remote_Sample/Remote_Sample/Source.cpp
+++ Remote_Sample/Remote_Sample/Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "GitHub!" << std::endl;
+	std::cout << "”š‚ğ“ü—Í"<< std::endl;
+	int a;
+	std::cin >> a;
+	std::cout << a<< std::endl;
 	return 0;
 }
